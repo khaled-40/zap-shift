d8d825bed8bc4be2b07e8e176f8990f35b5815f6
khaled-40 <khaledmasud.dev@gmail.com>
2025-12-17 11:50:26 +0600

    added brand image using swiper.js

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import image1 from '../assets/banner/banner1.png'
-import image2 from '../assets/banner/banner2.png'
-import image3 from '../assets/banner/banner3.png'
+import image1 from '../../assets/banner/banner1.png'
+import image2 from '../../assets/banner/banner2.png'
+import image3 from '../../assets/banner/banner3.png'
 
 const Banner = () => {
     return (
