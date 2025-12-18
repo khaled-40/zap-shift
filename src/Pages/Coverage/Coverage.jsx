34c1f17de1c7920591d4d1c0c27bf1bcacf7c30f
khaled-40 <khaledmasud.dev@gmail.com>
2025-12-18 22:51:57 +0600

    added search funtionality on coverage map

--- src/Pages/Coverage/Coverage.jsx
+++ src/Pages/Coverage/Coverage.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useLoaderData } from 'react-router';
 
 const Coverage = () => {
     const positions = useLoaderData();
+    const mapRef = useRef();
     console.log(positions)
-    const position = [23.8103, 90.4125]
+    const position = [23.8103, 90.4125];
+    const handleSearchLocation = (e) => {
+        e.preventDefault();
+        const location = e.target.location.value;
+        const district = positions.find(p => p.district.toLowerCase().includes(location.toLowerCase()));
+        if (district) {
+            const coord = [district.latitude, district.longitude];
+            console.log(coord)
+            mapRef.current.flyTo(coord, 14);
+        }
+    }
     return (
         <div className='pt-16'>
             <h2 className='text-3xl text-secondary font-bold'>We are available in 64 districts</h2>
             <div>
-
+                <form onSubmit={handleSearchLocation}>
+                    <label className="input">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input type="search" name='location' className="grow" placeholder="Search" />
+                    </label>
+                </form>
             </div>
             <div className='w-full h-200 '>
                 <MapContainer center={position}
                     zoom={7}
                     scrollWheelZoom={false}
+                    ref={mapRef}
                     className='h-112.5 rounded-xl'
                 >
                     <TileLayer
@@ -26,7 +54,7 @@ const Coverage = () => {
                     {
                         positions.map(position => <Marker key={position.district} position={[position.latitude, position.longitude]}>
                             <Popup>
-                                <strong>{position.district}</strong> <br /> 
+                                <strong>{position.district}</strong> <br />
                                 Service Area: {position.covered_area.join(', ')}
                             </Popup>
                         </Marker>)
