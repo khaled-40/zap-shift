6934137f988391d58991115d36c2b6dad892d9f5
khaled-40 <khaledmasud.dev@gmail.com>
2026-01-02 01:57:20 +0600

    implemented status change on approving the rider request

--- src/Pages/Dashboard/ApproveRiders/ApproveRiders.jsx
+++ src/Pages/Dashboard/ApproveRiders/ApproveRiders.jsx
@@ -3,19 +3,36 @@ import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import { FaRegTrashAlt, FaUserCheck } from 'react-icons/fa';
 import { IoPersonRemove } from 'react-icons/io5';
+import Swal from 'sweetalert2';
 
 const ApproveRiders = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: riders = [] } = useQuery({
+    const { refetch, data: riders = [] } = useQuery({
         queryKey: ['riders', 'pending'],
         queryFn: async () => {
             const res = await axiosSecure.get('/riders');
             return res.data;
         }
     })
-    
-    const handleApprove = id => {
-        console.log(id)
+
+    const handleApprove = (id, status,email) => {
+        console.log(email)
+        console.log(id, status);
+        const updateInfo = { status: status, email };
+        console.log(updateInfo)
+        axiosSecure.patch(`/riders/${id}`, updateInfo)
+            .then(res => {
+                if (res.data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: `This rider status has been ${status}`,
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            })
     }
     return (
         <div>
@@ -36,16 +53,17 @@ const ApproveRiders = () => {
                     <tbody>
                         {
                             riders.map((rider, index) => <tr key={rider._id}>
-                                <th>{index +1}</th>
+                                <th>{index + 1}</th>
                                 <td>{rider.riderName}</td>
                                 <td>{rider.riderEmail}</td>
                                 <td>{rider.riderDistrict}</td>
-                                <td>{rider.status}</td>
+                                <td className={`${rider.status === 'approved' ? 'badge badge-success  text-white'
+                                    : rider.status === 'rejected'?'badge badge-error  text-white':' text-black'} py-auto`}>{rider.status}</td>
                                 <td>
-                                    <button onClick={() => handleApprove(rider._id)} className='btn btn-sm'>
+                                    <button onClick={() => handleApprove(rider._id, 'approved',rider.riderEmail)} className='btn btn-sm'>
                                         <FaUserCheck />
                                     </button>
-                                    <button className='btn btn-sm mx-2'>
+                                    <button onClick={() => handleApprove(rider._id, 'rejected')} className='btn btn-sm mx-2'>
                                         <IoPersonRemove />
                                     </button>
                                     <button className='btn btn-sm'>
