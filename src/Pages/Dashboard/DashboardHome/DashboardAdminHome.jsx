399b7583563e623618190eab52999e91f8deee34
khaled-40 <khaledmasud.dev@gmail.com>
2026-01-14 00:08:44 +0600

    loaded deliveryStats information in the admin dashboard

--- src/Pages/Dashboard/DashboardHome/DashboardAdminHome.jsx
+++ src/Pages/Dashboard/DashboardHome/DashboardAdminHome.jsx
@@ -1,9 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const DashboardAdminHome = () => {
+    const axiosSecure = useAxiosSecure();
+    const { data: deliveryStats = [] } = useQuery({
+        queryKey: ['delivery-status-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/parcels/delivery-status/stats');
+            return res.data;
+        }
+    })
     return (
         <div>
             <h2>This is admin. Pay respect</h2>
+            <div className="stats shadow">
+                {
+                    deliveryStats.map((stat,index) => <div key={index} className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-8 w-8 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <div className="stat-title">{stat._id}</div>
+                    <div className="stat-value">{stat.count}</div>
+                    <div className="stat-desc">Jan 1st - Feb 1st</div>
+                </div>)
+                }
+                
+            </div>
         </div>
     );
 };
